9c52878a8a6131c935256f230895dc325d5be0cc
Alexander <iguminov05@yandex.ru>
2022-09-18 16:04:16 +0300

    Example React+TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import React, {createElement as e, useState} from 'react';
+import { ErrorMessage } from './components/ErrorMessage';
+import { Loader } from './components/Loader';
+import {Product} from './components/Product'
+import { useProducts } from './hooks/products';
+import {Modal} from './components/Modal'
+import {CreateProduct} from './components/CreateProduct'
+
 
 
 function App() {
+  const {loading, error, products} = useProducts()
 
-
+  
   return (
     <div className="container mx-auto max-w-2xl pt-5">
-        qwertyuio
+      {loading && <Loader/>}
+      {error && <ErrorMessage error={error} />}
+
+      {products.map(product => <Product product={product} key={product.id}/>)}    
+
+      <Modal title='Create new product'>
+        <CreateProduct/>
+      </Modal> 
 
     </div>
   )
